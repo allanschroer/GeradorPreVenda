2bf4a33b964f7dd3ea1de99e29e6bffba3c5db5e
Allan Schroer <56697839+allanschroer@users.noreply.github.com>
2021-06-21 18:28:31 -0300

    Incluida opção de gerar várias pre-vendas, opção 2

--- GeradorPreVenda/Gravar.cs
+++ GeradorPreVenda/Gravar.cs
@@ -7,13 +7,22 @@ namespace GeradorPreVenda
     class gravar
     {
 
-        public void Gravar(string prevenda, string anotacao)
+        public void GravarComAnotacao(string prevenda, string anotacao)
         {
             string texto = prevenda + " - " + anotacao + Environment.NewLine;
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            File.WriteAllText(Path.Combine(docPath, "Prevendas.txt"), texto);
+            File.WriteAllText(Path.Combine(docPath, "Prevendas Com Descrição.txt"), texto);
             string[] linhas = { texto };
             File.AppendAllLines(Path.Combine("Prevendas.txt"), linhas);
+        }
+
+        public void GravarVariasPreVendas(string prevendas)
+        {
+            string texto = prevendas + Environment.NewLine;
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            File.WriteAllText(Path.Combine(docPath, "PrevendasAleatorias.txt"), texto);
+            string[] linhas = { texto };
+            File.AppendAllLines(Path.Combine("PrevendasAleatorias.txt"), linhas);
         }
     }
 }
--- GeradorPreVenda/Program.cs
+++ GeradorPreVenda/Program.cs
@@ -11,7 +11,10 @@ namespace GeradorPreVenda
             while (rodando)
             {
                 Console.Clear();
-                Console.Write("[    MENU DE PRÉ-VENDA   ]\n\n1 - Criar nova pré-venda\n0 - Sair\n\n=>");
+                Console.Write("[    MENU DE PRÉ-VENDA   ]\n" +
+                    "\n1 - Criar nova pré-venda com anotação" +
+                    "\n2 - Gerar várias pré-vendas aleatórias" +
+                    "\n0 - Sair" + "\n\n=>");
                 int opcao = int.Parse(Console.ReadLine());
 
                 switch (opcao)
@@ -21,14 +24,25 @@ namespace GeradorPreVenda
                         gravar gravador = new gravar();
                         Random ranNum = new Random();
                         string prevenda = "HAV" + ranNum.Next(100000, 999999) + "-" + ranNum.Next(10, 99);
-
-
                         Console.Write("---------> " + prevenda + "\nDigite a anotação para a pré-venda: ");
                         string anotacao = Console.ReadLine();
-
-                        gravador.Gravar(prevenda, anotacao);
-
+                        gravador.GravarComAnotacao(prevenda, anotacao);
                         break;
+
+                    case 2:
+                        Random ranNumVarias = new Random();
+                        Console.Clear();
+                        gravar varias = new gravar();
+                        Console.Write("Quantas Pre-vendas deseja gerar? -> ");
+                        int quantiade = int.Parse(Console.ReadLine());
+                        for (int i = 0; i <= quantiade; i++)
+                        {
+                            string prevendas = "HAV" + ranNumVarias.Next(100000, 999999) + "-" + ranNumVarias.Next(10, 99);
+                            varias.GravarVariasPreVendas(prevendas);
+                        }
+                        Console.ReadLine();
+                        break;
+
                     case 0:
                         rodando = false;
                         break;
